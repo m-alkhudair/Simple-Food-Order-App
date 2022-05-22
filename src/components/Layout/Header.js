c5661cccfb573bc11c9ce9120457a4b52a18df5e
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-05-22 18:17:56 +0300

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import classes from './Header.module.css';
+import meals from '../../assets/meals.jpeg';
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+          {/* we can also use a url if the image isnt local in that case we'll use "" instead of curly braces */}
+          <img src={meals} alt="A table full of delicious food!"/>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
