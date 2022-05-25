bcfd6ac6ad0dc5ba04eb8804a0a00f9330868f15
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-05-25 15:31:49 +0300

    set up useReducer to manage cart state with context

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -1,13 +1,47 @@
+import { act } from "@testing-library/react";
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+// the state arg. is latest state snapshot and is provided by react automatically
+// the first arg. of the dispatch function will be in the action arg. below
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    // conact() because we want to always edit array in immutable ways, because we dont want to change the orginal array instead of editing the old array in memory so we return a new one
+    const updatedItems = state.items.concat(action.payload);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", payload: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", payload: id });
+  };
 
   // real concrete object that is our context that we will maipulate and use
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
