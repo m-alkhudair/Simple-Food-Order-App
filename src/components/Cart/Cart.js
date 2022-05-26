3daf3350d75ed8fd14f306b8f86ef886fbcb0c59
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-05-26 17:58:24 +0300

    set up and populated the cart and added functionality with CartContext

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,32 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
+import CartContext from "../../Store/cart-context";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hadItems = cartCtx.items.length > 1;
+
+  const cartItemAddHandler = item => {};
+  const cartItemRemoveHandler = id => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          // the item passed into this function below is the item inside the map function
+          // the bind method is to ensure that we pass these arguments into the handler functions. Bind() pre-configures a function for future execution and to pre-configure the arguments to be passed
+          onAdd={cartItemAddHandler.bind(null, item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
@@ -15,13 +36,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hadItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
