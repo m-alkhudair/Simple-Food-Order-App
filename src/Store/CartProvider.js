ff6d3266673196af3b9c02b3ecdacd4374e96b81
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-06-06 10:13:33 +0300

    coplex reducer logic to check if new item in cart already exists

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -11,10 +11,32 @@ const defaultCartState = {
 // the first arg. of the dispatch function will be in the action arg. below
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    
+    // this is the first step in checking whether the newly added item exists aleardy
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.item[existingCartItemIndex];
+    let updatedItem;
+    let updatedItems;
+
+    // check if truthy
+    if (existingCartItem) {
+      updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      }
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      // the case its a brand new item
+      updatedItem = {...action.item};
+      updatedItems = state.items.concat(updatedItem);
+    }
+
     // conact() because we want to always edit array in immutable ways, because we dont want to change the orginal array instead of editing the old array in memory so we return a new one
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount =
-      state.totalAmount + action.item.price * action.item.amount;
+    // const updatedItems = state.items.concat(action.item);
 
     return {
       items: updatedItems,
