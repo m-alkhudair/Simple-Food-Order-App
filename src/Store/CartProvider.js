90c0931d0317073fbdfc652aab1b9455549a6c0e
Mishaal Alkhudair <m.alkhudair.test@gmail.com>
2022-06-07 12:53:25 +0300

    remove functionality for the minus button in the cart

--- src/Store/CartProvider.js
+++ src/Store/CartProvider.js
@@ -11,8 +11,9 @@ const defaultCartState = {
 // the first arg. of the dispatch function will be in the action arg. below
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
-    
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
     // this is the first step in checking whether the newly added item exists aleardy
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
@@ -25,8 +26,8 @@ const cartReducer = (state, action) => {
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
-        amount: existingCartItem.amount + action.item.amount
-      }
+        amount: existingCartItem.amount + action.item.amount,
+      };
       updatedItems = [...state.items];
       // overriding the old item in the array with the updated item
       updatedItems[existingCartItemIndex] = updatedItem;
@@ -35,7 +36,6 @@ const cartReducer = (state, action) => {
       // updatedItem = {...action.item};
       // updatedItems = state.items.concat(updatedItem);
       updatedItems = state.items.concat(action.item);
-
     }
 
     // conact() because we want to always edit array in immutable ways, because we dont want to change the orginal array instead of editing the old array in memory so we return a new one
@@ -46,6 +46,38 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    // if it is the last item on the cart that we want to remove, else not
+    if (existingItem.amount === 1) {
+      // to filer out the entire itme if the amount is just 1 from the cart
+      // the one item the item to be removed the filter function will return false otherwise true for the rest of the items and that item will be removed in the new array
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingItem,
+        amount: existingItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      // overriding the old item in the array with the updated item that has the updated amount!
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+
+  // this below is a fallback incase we had another action type not accounted for
   return defaultCartState;
 };
 
@@ -59,7 +91,7 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "ADD", item: item });
   };
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: "REMOVE", item: id });
+    dispatchCartAction({ type: "REMOVE", id: id });
   };
 
   // real concrete object that is our context that we will maipulate and use
